f8044c4c5aa977757150dc2f0f32734bd160a5ca
javierlopezg00 <javierlopezguzman00@gmail.com>
2022-03-20 23:25:18 -0600

    avances create de electronicos personalizados

--- servidor/models/Electronico.js
+++ servidor/models/Electronico.js
@@ -32,6 +32,50 @@ const ElectronicoSchema = mongoose.Schema({
     descripcion: {
         type: String,
         default: Date.now()
+    },
+    resolucion: {
+        type: String,
+        default: Date.now()
+    },
+    bits: {
+        type: String,
+        default: Date.now()
+    },
+    pulgadas: {
+        type: String,
+        default: Date.now()
+    },
+    hdmi: {
+        type: String,
+        default: Date.now()
+    },
+    pulgadasReloj: {
+        type: String,
+        default: Date.now()
+    },
+    sistemaOperativo: {
+        type: String,
+        default: Date.now()
+    },
+    ram: {
+        type: String,
+        default: Date.now()
+    },
+    memoria: {
+        type: String,
+        default: Date.now()
+    },
+    maximoJugadores: {
+        type: String,
+        default: Date.now()
+    },
+    graficos: {
+        type: String,
+        default: Date.now()
+    },
+    consola: {
+        type: String,
+        default: Date.now()
     }
 })
 module.exports = mongoose.model('Electronico', ElectronicoSchema);
