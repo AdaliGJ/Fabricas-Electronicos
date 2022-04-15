1d6a1b2d8c4209f87d3e27350edb1d309c60b3c3
Jorge-E-Guerrero <guerrero191096@unis.edu.gt>
2022-04-15 16:35:09 -0600

    Kill Node_modules >:´D

--- servidor/routes/garantia.js
+++ servidor/routes/garantia.js
@@ -10,10 +10,7 @@ router.put('/:id', garantiasController.actualizarGarantia);
 router.get('/:id', garantiasController.obtenerGarantia);
 router.get('/dispositivo/:id', garantiasController.verificarGarantia);
 router.delete('/:id', garantiasController.borrarGarantia);
-<<<<<<< HEAD
 router.get('/devolver/:id', garantiasController.verificarGarantia);
-=======
->>>>>>> 7c481d873ec26e8823873c2871075b80f7ff8519
 //router.delete('/:id', clienteController.borrarCliente);
 
 module.exports =  router;
